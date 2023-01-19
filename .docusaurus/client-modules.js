63710d1fce9ce2e24e7deb1d78b89609d3543901
Grudge10 <100700937+Grudge10@users.noreply.github.com>
2023-01-19 14:35:04 +0800

    created the website

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/Users/Grudge/Coding/marigolds-secret/node_modules/infima/dist/css/default/default.css'),
+  require('/Users/Grudge/Coding/marigolds-secret/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/Users/Grudge/Coding/marigolds-secret/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/Users/Grudge/Coding/marigolds-secret/src/css/custom.css'),
+];
